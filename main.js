9919dbc218fb2bda9edd5587bcb980bbad1f7bc7
marciorobertomr <marcio_mr1@hotmail.com>
2024-09-08 19:51:46 -0300

    Criação do Modules

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+import { calculate } from "./calculate.js";
+import { changeTheme } from "./changeTheme.js";
+import copy from "./copy.js";
+
 const main = document.querySelector("main");
 const root = document.querySelector(":root");
 const input = document.getElementById("input");
@@ -18,7 +22,7 @@ input.addEventListener('keydown', (ev) => {
     }
 
     if (ev.key === 'Enter') {
-        calculate();
+        calculate(resultInput, input);
     }
 })
 
@@ -36,39 +40,14 @@ document.getElementById('clear').addEventListener('click', () => {
     resultInput.classList.remove('error');
 })
 
-document.getElementById('equal').addEventListener('click', calculate);
-
-function calculate() {
-    resultInput.value = 'ERROR';
-    resultInput.classList.add('error');
-    resultInput.value = eval(input.value);
-    resultInput.classList.remove('error');
-}
+document.getElementById('equal').addEventListener('click', () => {
+    calculate(resultInput, input)
+});
 
 document.getElementById('themeSwitcher').addEventListener('click', () => {
-    if (main.dataset.theme === 'dark') {
-        root.style.setProperty('--bg-color', '#f1f5f9');
-        root.style.setProperty('--border-color', '#aaa');
-        root.style.setProperty('--font-color', '#212529');
-        root.style.setProperty('--primary-color', '#26834a');
-        main.dataset.theme = 'light';
-    } else {
-        root.style.setProperty('--bg-color', '#212529');
-        root.style.setProperty('--border-color', '#667');
-        root.style.setProperty('--font-color', '#f1f5f9');
-        root.style.setProperty('--primary-color', '#4dff91');
-        main.dataset.theme = 'dark';
-    }
+    changeTheme(main, root);
 })
 
 document.getElementById('copyToClipboard').addEventListener('click', (ev) => {
-    const button = ev.currentTarget;
-    if (button.innerText === 'Copy') {
-        button.innerText = "Copied!";
-        button.classList.add('success');
-        navigator.clipboard.writeText(resultInput.value);
-    } else {
-        button.innerText = 'Copy';
-        button.classList.remove('success');
-    }
+    copy(ev, resultInput);
 })
