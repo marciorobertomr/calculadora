388e07f032dfd37a1956010af31e72b0d8efa330
Márcio Roberto <111321281+marciorobertomr@users.noreply.github.com>
2025-06-20 19:54:48 -0300

    Delete calculadora-main directory

--- calculadora-main/calculate.js
+++
@@ -1,9 +0,0 @@
-function calculate(resultInput, input) {
-    resultInput.value = 'ERROR';
-    resultInput.classList.add('error');
-    resultInput.value = eval(input.value);
-    resultInput.classList.remove('error');
-}
-
-
-export { calculate };
